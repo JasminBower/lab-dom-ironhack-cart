56ba21f35acfcc28ef8441e69de73522086a68bd
JasminBower <jasmin.ibower@gmail.com>
2022-02-22 08:56:57 +0100

    lab not quite but almost done

--- js/index.js
+++ js/index.js
@@ -2,26 +2,45 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const  quantity = product.querySelector('.quantity input');
+  const subtotal = product.querySelector('.subtotal span')
+  const priceValue = Number(price.innerText);
+  const valueQuantity = Number(quantity.value);
 
-  //... your code goes here
+  let totalPriceOfProduct = priceValue * valueQuantity;
+  subtotal.innerText = totalPriceOfProduct;
+  
+  return totalPriceOfProduct;
+
+ 
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
+  let totalSum = 0;
 
   // ITERATION 2
-  //... your code goes here
+  const getProducts = document.querySelectorAll('.product');
+
+  getProducts.forEach(product => {
+         subtotal = updateSubtotal(product)
+         totalSum += subtotal;
+  });
+  
 
   // ITERATION 3
-  //... your code goes here
+  const totalPrice  = document.querySelector('#total-value span');
+ 
+  totalPrice.innerText = totalSum;
 }
 
 // ITERATION 4
-
+///// IM SORRY THIS IS WHERE MY BRAIN TURNED INTO NOODLES AND I HAD TO STOP 🥲 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
